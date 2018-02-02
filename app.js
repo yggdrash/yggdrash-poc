f2b48bcb44837a5b2b0151515a99cc590e34c6b0
DongOk Ryu <poprdo@gmail.com>
2018-02-02 15:58:41 +0900

    add layout

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('title', 'Yggdrash Browser');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
