07cc7e7e0e405ee14ed69e74d77719feafc75a04
DongOk Ryu <poprdo@gmail.com>
2018-02-25 01:45:40 +0900

    모델을 만들고, 분리 합시다. gogogo

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
-
-const contents = [
-  {name:'contest1', address:'d04b98f48e8f8bcc15c6ae5ac050801cd6dcfd428fb5f9e65c4e16e7807340fa'},
-  {name:'contest2', address:'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'}
-];
+var leger = require('../model/stemchain.json');
+var yeed = require('../model/yeedchain.json');
+var sacred = require('../model/sacredwaterchain.json');
 
     /* GET home page. */
 router.get('/', function(req, res, next) {
-
-
-
-  res.render('index',
-      {
+  res.render('index', {
         title: 'Yggdrash Browser',
-        content: contents
-      }
-      );
+        ledger: leger,
+        yeed: yeed,
+          sacred: sacred
+      });
 });
 
 module.exports = router;
